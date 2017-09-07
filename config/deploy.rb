3092c42df482c5a64b357059a2c9faa99f858679
Paul Martensen <paul.martensen@gmx.de>
2017-09-07 11:13:06 +0200

    fixed app name.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 # config valid only for current version of Capistrano
 lock "3.9.0"
 
-set :application, "Brewing Bits"
+set :application, "Brewing_Bits"
 set :repo_url, "git@bitbucket.org:Haniyya/brewing-bits.git"
 set :repository, '_site'
 set :scm, :none
